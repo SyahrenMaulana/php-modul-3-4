d3f40ecad2eaa92c004ed854e5644b74066c40f5
SyahrenMaulana <sfa.asus@gmail.com>
2021-09-09 06:24:45 +0700

    first commit

---
+++ latihan 3.php
@@ -0,0 +1,10 @@
+<?php
+    $user = "Firdausa";
+    $pass = "123";
+
+    if($user == "Firdausa" && $pass == "123"){
+        echo "Login Berhasil";
+    } else {
+        echo "Login Gagal";
+    }
+?>
